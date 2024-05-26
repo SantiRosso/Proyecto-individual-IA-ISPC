29dee4b3ee6037f34934bc7ee8b8d4342e01a70a
Santiago Rosso <santiagorosso@live.com.ar>
2024-05-26 20:53:43 -0300

    Añadir solución de ejercición 3 de la actividad 5.1

---
+++ actividad_5.1/ejercicio3.psc
@@ -0,0 +1,6 @@
+INICIO
+    LEER numero
+    SI numero >= 10 Y numero <= 20 ENTONCES
+        IMPRIMIR "El número está en el rango [10, 20]."
+    FIN SI
+FIN
---
+++ actividad_5.1/ejercicio3.py
@@ -0,0 +1,10 @@
+# numero_en_rango.py
+# Este programa verifica si un número está en el rango [10, 20].
+
+# Entrada
+numero = float(input("Ingrese un número: "))
+
+# Proceso y Salida
+if 10 <= numero <= 20:
+    print("El número está en el rango [10, 20].")
+
