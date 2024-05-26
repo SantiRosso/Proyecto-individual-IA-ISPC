7cc80be510719815cedc547fbfefef4927056dda
Santiago Rosso <santiagorosso@live.com.ar>
2024-05-26 20:57:15 -0300

    Añadir solución de ejercición 4 de la actividad 5.1

---
+++ actividad_5.1/ejercicio4.psc
@@ -0,0 +1,8 @@
+INICIO
+    LEER numero
+    SI (numero % 2 = 0 O numero % 5 = 0) ENTONCES
+        IMPRIMIR "El número es divisible por 2 o por 5."
+    SINO
+        IMPRIMIR "El número no es divisible por 2 ni por 5."
+    FIN SI
+FIN
---
+++ actividad_5.1/ejercicio4.py
@@ -0,0 +1,11 @@
+# divisible_por_2_o_5.py
+# Este programa determina si un número es divisible por 2 o por 5.
+
+# Entrada
+numero = int(input("Ingrese un número: "))
+
+# Proceso y Salida
+if numero % 2 == 0 or numero % 5 == 0:
+    print("El número es divisible por 2 o por 5.")
+else:
+    print("El número no es divisible por 2 ni por 5.")
