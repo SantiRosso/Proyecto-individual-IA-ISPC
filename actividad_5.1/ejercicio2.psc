2888ae7d7341b29a6dd9485f8703c08288396dcd
Santiago Rosso <santiagorosso@live.com.ar>
2024-05-26 20:52:10 -0300

    Añadir solución de ejercición 2 de la actividad 5.1

---
+++ actividad_5.1/ejercicio2.psc
@@ -0,0 +1,8 @@
+INICIO
+    LEER numero
+    SI numero >= 0 ENTONCES
+        IMPRIMIR "El número es positivo."
+    SINO
+        IMPRIMIR "El número es negativo."
+    FIN SI
+FIN
---
+++ actividad_5.1/ejercicio2.py
@@ -0,0 +1,11 @@
+# numero_positivo_negativo.py
+# Este programa determina si un número es positivo o negativo.
+
+# Entrada
+numero = float(input("Ingrese un número: "))
+
+# Proceso y Salida
+if numero >= 0:
+    print("El número es positivo.")
+else:
+    print("El número es negativo.")
