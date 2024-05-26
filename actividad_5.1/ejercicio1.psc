26b924190116b4c4ddb057c6d3f2ded620ff9637
Santiago Rosso <santiagorosso@live.com.ar>
2024-05-26 20:50:53 -0300

    Añadir solución de ejercición 1 de la actividad 5.1

---
+++ actividad_5.1/ejercicio1.psc
@@ -0,0 +1,6 @@
+INICIO
+    LEER edad
+    SI edad >= 18 ENTONCES
+        IMPRIMIR "Eres mayor de edad."
+    FIN SI
+FIN
---
+++ actividad_5.1/ejercicio1.py
@@ -0,0 +1,10 @@
+# mayor_edad.py
+# Este programa determina si una persona es mayor de edad.
+
+# Entrada
+edad = int(input("Ingrese su edad: "))
+
+# Proceso y Salida
+if edad >= 18:
+    print("Eres mayor de edad.")
+
